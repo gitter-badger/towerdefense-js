d50810e872a905557e1782a34bdb04558cd70ce4
qindiandefence <qindiandefence@gmail.com>
2014-10-21 11:46:17 -0400

    Updates

--- gamelogic.js
+++ gamelogic.js
@@ -16,7 +16,7 @@ var Game = {
 	canvas: null,
 	get context() {
 		return ctx;
-	}
+	},
 	
 	// Game variables and constants.
     towers: [],
@@ -37,6 +37,44 @@ var Game = {
     // Used for clearInterval() for pausing.
     looper: null
 };
+// Utility Functions
+function init() {
+	ctx = $('#canvas')[0].getContext("2d");
+  	WIDTH = $("#canvas").width();
+ 	HEIGHT = $("#canvas").height();
+  	
+}
+function fill(r, g, b) {
+	if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255) {
+		return;
+	}
+	ctx.fillStyle = "#" + r.toString(16) + g.toString(16) + b.toString(16);
+}
+function circle(x,y,r) {
+  	ctx.beginPath();
+  	ctx.arc(x, y, r, 0, Math.PI*2, true);
+  	ctx.closePath();
+  	ctx.fill();
+}
+function rect(x,y,w,h) {
+  	ctx.beginPath();
+  	ctx.rect(x,y,w,h);
+  	ctx.closePath();
+  	ctx.fill();
+}
+function clear() {
+  	ctx.clearRect(0, 0, WIDTH, HEIGHT);
+}
+
+
+Game.drawPath = function() {
+	for (var tX = 0; tX < this.boardWidth; tX++) {
+		for (var tY = 0; tY < this.boardHeight; tY++) {
+			
+		}
+	}
+};
+
 
 Game.update = function() {
 	
@@ -45,31 +83,6 @@ Game.update = function() {
 Game.render = function() {
 	
 };
-
-function init() {
-	ctx = $('#canvas')[0].getContext("2d");
-  	WIDTH = $("#canvas").width();
- 	HEIGHT = $("#canvas").height();
-  	
-}
-
-function circle(x,y,r) {
-  	ctx.beginPath();
-  	ctx.arc(x, y, r, 0, Math.PI*2, true);
-  	ctx.closePath();
-  	ctx.fill();
-}
-
-function rect(x,y,w,h) {
-  	ctx.beginPath();
-  	ctx.rect(x,y,w,h);
-  	ctx.closePath();
-  	ctx.fill();
-}
-
-function clear() {
-  	ctx.clearRect(0, 0, WIDTH, HEIGHT);
-}
 
 
 
@@ -85,7 +98,9 @@ function startGame() {
 	Game.shots = [];
 	
 }
-
+function resume() {
+	setInterval(Game.looper, 33);
+}
 function pause() {
 	if (Game.looper == null) {
 		return false;
