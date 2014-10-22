71c61a9087ae6d0bb1d67ad5c5e0eb776a3517f7
qindiandefence <qindiandefence@gmail.com>
2014-10-21 20:11:21 -0400

    Moved several functions to utility.js

--- gamelogic.js
+++ gamelogic.js
@@ -9,7 +9,6 @@ var HEIGHT;
 var ctx;
 
 
-
 var Game = {
 	// Used for loading the game.
 	loaded: false,
@@ -23,8 +22,8 @@ var Game = {
     enemies: [],
     shots: [],
     
-    money: 500,
-    lives: 50,
+    money: null,
+    lives: null,
     
     track: {
     	start: [0, 0],
@@ -37,40 +36,18 @@ var Game = {
     // Used for clearInterval() for pausing.
     looper: null
 };
-// Utility Functions
-function init() {
-	ctx = $('#canvas')[0].getContext("2d");
-  	WIDTH = $("#canvas").width();
- 	HEIGHT = $("#canvas").height();
-  	
-}
-function fill(r, g, b) {
-	if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255) {
-		return;
-	}
-	ctx.fillStyle = "#" + r.toString(16) + g.toString(16) + b.toString(16);
-}
-function circle(x,y,r) {
-  	ctx.beginPath();
-  	ctx.arc(x, y, r, 0, Math.PI*2, true);
-  	ctx.closePath();
-  	ctx.fill();
-}
-function rect(x,y,w,h) {
-  	ctx.beginPath();
-  	ctx.rect(x,y,w,h);
-  	ctx.closePath();
-  	ctx.fill();
-}
-function clear() {
-  	ctx.clearRect(0, 0, WIDTH, HEIGHT);
-}
+
 
 
 Game.drawPath = function() {
+	var bWidth = 600, bHeight = 400;
+	var tileWidth = bWidth / this.boardWidth;
 	for (var tX = 0; tX < this.boardWidth; tX++) {
 		for (var tY = 0; tY < this.boardHeight; tY++) {
-			
+			var xpos = 100 + tX * 100;
+			var ypos = 100 + tY * 100;
+			fill(255, 255, 255);
+			rect(xpos, ypos, 100, 100);
 		}
 	}
 };
@@ -89,21 +66,14 @@ Game.render = function() {
 window.onload = function() {
 	Game.loaded = true;
 	Game.canvas = document.getElementById("#game");
-	Game.context = canvas.getContext('2d');
+	init();
 }
 
 function startGame() {
 	Game.towers = [];
 	Game.enemies = [];
 	Game.shots = [];
-	
-}
-function resume() {
-	setInterval(Game.looper, 33);
-}
-function pause() {
-	if (Game.looper == null) {
-		return false;
-	}
-	clearInterval(Game.looper);
+	Game.money = 500;
+	Game.lives = 50;
+	play();
 }
